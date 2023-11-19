428c6e8a34135eb71325d3261d09d8ec790fb55a
Vladyslav Pavlenko <vut1111111@gmail.com>
2023-11-19 13:45:48 +0200

    Add project files.

---
+++ CryptoLaba3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+
+byte[] masterKey = GenerateRandomBytes(32);
+
+byte[] sessionKey = GenerateSessionKey(masterKey);
+
+Console.WriteLine("Master Key: " + Convert.ToBase64String(masterKey));
+Console.WriteLine("Session Key: " + Convert.ToBase64String(sessionKey));
+
+byte[] GenerateRandomBytes(int length)
+{
+    using var rng = new RNGCryptoServiceProvider();
+    var randomBytes = new byte[length];
+    rng.GetBytes(randomBytes);
+
+    return randomBytes;
+}
+
+byte[] GenerateSessionKey(byte[] masterKey)
+{
+    using Aes aes = Aes.Create();
+    aes.Key = masterKey;
+    aes.GenerateIV();
+
+    using ICryptoTransform encryptor = aes.CreateEncryptor();
+    byte[] encryptedSessionKey = encryptor.TransformFinalBlock(masterKey, 0, masterKey.Length);
+
+    return encryptedSessionKey;
+}
